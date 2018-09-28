88541ae53e202c8fb30b151e63fbe8dee477c2ad
Gabriel <gabrielsantossfc@gmail.com>
2018-09-28 16:02:38 -0300

    Criamos novos métodos para a class usuario

--- index.php
+++ index.php
@@ -1,11 +1,26 @@
 <?php 
 
 require_once("config.php");
+//carrega 1 usuario
+//$Gabriel = new Usuario();
+//$Gabriel->loadbyId(1);
+//echo $Gabriel;
 
-$Gabriel = new Usuario();
+//carrega uma lista de usuarios
+//$lista = Usuario::getList();
+//echo json_encode($lista);
 
-$Gabriel->loadbyId(1);
+//carrega lista de usuarios buscando pelo login
+//$search = Usuario::search("Ga");
+//echo json_encode($search);
 
-echo $Gabriel;
+//carrega um usuário usando o login e a senha
+
+$usuario = new Usuario();
+
+$usuario->login("Gabriel", "3273082");
+
+
+echo $usuario;
 
  ?>
